3c72bc473d6e4782c75796d93a361d3df6cf3315
Amridasri2276 <162562388+Amridasri2276@users.noreply.github.com>
2024-06-01 12:23:40 +0530

    Add files via upload

---
+++ Prob1.js
@@ -0,0 +1,55 @@
+import logo from './logo.svg';
+import './App.css';
+import Reactimport { useState } from 'react';
+{useState} from 'react';
+import './App.css'
+function App() {
+  return (
+    import React,{useState} from 'react'
+    import './App.css'
+    function App()
+    {
+      return (
+        <div className="container">
+        <div className="calculator">
+        <form actions="">
+        <div>
+        <input type ="text"/>
+        </div>
+        <div>
+        <input type ="button" value="AC"/>
+        <input type ="button" value="DE"/>
+        <input type ="button" value="."/>
+        <input type ="button" value="/"/>
+
+        </div>
+        <div>
+        <input type ="button" value="7"/>
+        <input type ="button" value="8"/>
+        <input type ="button" value="9"/>
+        <input type ="button" value="*"/>
+
+        </div>
+        </div>
+        <div>
+        <input type ="button" value="4"/>
+        <input type ="button" value="5"/>
+        <input type ="button" value="6"/>
+        <input type ="button" value="+"/>
+
+        </div>
+        <div>
+        <input type ="button" value="1"/>
+        <input type ="button" value="2"/>
+        <input type ="button" value="3"/>
+        <input type ="button" value="-"/>
+
+        </div>
+        
+        </form>
+        </div>
+        <
+  );
+}
+
+export default App;
